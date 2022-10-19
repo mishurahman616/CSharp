66050bd29fbf29f88a9397d2c5b193ac56c07b6e
Mishu Rahman <mishu.cse616@gmail.com>
2022-10-20 02:14:35 +0600

    Solved task1 and task2

---
+++ Assignment1/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Enter the length of the array: ");
+int arrayLength = Int16.Parse(Console.ReadLine());
+int[] sum = new int[2 * arrayLength + 2];
+if (arrayLength >= 3 && arrayLength <= 1000)
+{
+    int[,] arr = new int[arrayLength, arrayLength];
+    
+    //Taking input array element 
+    for(int row= 0; row < arrayLength; row++)
+    {
+        for(int col= 0; col < arrayLength; col++)
+        {
+            Console.Write($"\nEnter the array element array[{row},{col}]: ");
+            arr[row,col] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    //Calculating sum
+    for (int row = 0; row < arrayLength; row++)
+    {
+        for (int col = 0; col < arrayLength; col++)
+        {
+            sum[row] += arr[row, col];
+            sum[arrayLength + col] += arr[row, col];
+            if (row == col)
+            {
+                sum[^2] += arr[row, col];
+            }
+            if (row == (arrayLength - 1) - col)
+            {
+                sum[^1] += arr[row, col];
+            }
+        }
+    }
+
+    //Finding maximum sum
+    int max = int.MinValue;
+    foreach(int i in sum)
+    {
+
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    Console.WriteLine($"Maximum sum is {max}.");
+}
---
+++ Assignment1/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("How many numbers are there? ");
+int n = int.Parse(Console.ReadLine());
+int largest = int.MinValue, secondLargest = int.MinValue, thirdLargest = int.MinValue;
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    if (number > largest)
+    {
+        thirdLargest = secondLargest;
+        secondLargest = largest;
+        largest = number;
+    }
+    if (number > secondLargest && number < largest)
+    {
+        thirdLargest = secondLargest;
+        secondLargest = number;
+    }
+    if (number > thirdLargest && number < secondLargest)
+    {
+        thirdLargest = number;
+    }
+}
+
+if(thirdLargest != int.MinValue)
+Console.WriteLine(thirdLargest);
+
