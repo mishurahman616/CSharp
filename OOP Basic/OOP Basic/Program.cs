73e269beb92c1e8d06fbd392c1edbf46520ccb4c
Mishu Rahman <mishu.cse616@gmail.com>
2022-10-20 23:21:03 +0600

    OOP Basic Class 7

---
+++ OOP Basic/OOP Basic/Program.cs
@@ -0,0 +1,9 @@
+﻿using OOP_Basic;
+
+WaterBottle waterBottle = new WaterBottle("Red", 200);
+waterBottle.WaterAmount = 100;
+waterBottle.AddWater(5);
+Console.WriteLine(waterBottle.WaterAmount);
+Console.WriteLine(waterBottle.Color);
+Console.WriteLine(waterBottle.IsOpen);
+waterBottle.ShowBottoleInformation();
