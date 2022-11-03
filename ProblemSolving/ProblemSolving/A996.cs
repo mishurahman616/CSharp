a86c57fad07634731a7b8dc42e1b28a111ee1a43
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-03 19:51:04 +0600

    Codeforces-996A - Hit the Lottery

---
+++ ProblemSolving/ProblemSolving/A996.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class A996
+    {
+        ////Solution of the codeforces problem 996A - Hit the Lottery
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int bill = 0;
+            bill += n / 100;
+            n%=100;
+            bill += n / 20;
+            n%=20;
+            bill += n / 10;
+            n%=10;
+            bill += n / 5;
+            n%=5;
+            bill += n;
+
+            Console.WriteLine(bill);
+        }
+    }
+}
--- ProblemSolving/ProblemSolving/Program.cs
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ProblemSolving;
 
-A443.Sovle();
+A996.Solve();
