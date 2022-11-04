85033b1e8df83bfd449407d5974451f0884da63f
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-04 20:37:35 +0600

    Codeforces-1328A - Divisibility Problem Solved

---
+++ Assignment2/ProblemSolving/1328A/Program.cs
@@ -0,0 +1,8 @@
+﻿// Solution of the codeforces problem 1328A - Divisibility Problem
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    var data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    var move = data[1] - (data[0] % data[1]);
+    Console.WriteLine(move == data[1]?0:move);
+}
