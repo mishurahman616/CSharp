205009e055207af520ee68bbe4513447f2a5849a
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-05 03:58:49 +0600

    Codeforces-1343B - Balanced Array Solved

---
+++ Assignment2/ProblemSolving/1343B/Program.cs
@@ -0,0 +1,34 @@
+﻿// Solution of the codeforces problem 1343B - Balanced Array
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    if((n/2)%2 != 0)
+    {
+        Console.WriteLine("NO");
+    }
+    else
+    {
+        Console.WriteLine("YES");
+        int k = n / 2;
+        int even = 0;
+        int odd = -1;
+        int evenSum = 0;
+        int oddSum = 0;
+        for(int j = 0; j < k; j++)
+        {
+            even += 2;
+            evenSum += even;
+            Console.Write($"{even} " );
+            
+        }
+        for(int j=k; j < n-1; j++)
+        {
+            odd += 2;
+            oddSum += odd;
+            Console.Write($"{odd} ");
+           
+        }
+        Console.WriteLine(evenSum-oddSum);
+    }
+}
