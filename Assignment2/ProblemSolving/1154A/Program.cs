d1d70230864b78f13f50840e455af92312897426
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-04 15:23:27 +0600

    Codeforces-1154A - Restoring Three Numbers Solved

---
+++ Assignment2/ProblemSolving/1154A/Program.cs
@@ -0,0 +1,6 @@
+﻿//Solution of the codeforces problem 1154A - Restoring Three Numbers
+
+
+int[] x =  Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Array.Sort(x);
+Console.WriteLine($"{x[3] - x[0]} {x[3] - x[1]} {x[3] - x[2]}");
