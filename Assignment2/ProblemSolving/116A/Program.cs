95ff35dd4705ba429b64bc89434571904887ba39
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-04 19:33:19 +0600

    Codeforces-116A - Tram solved

---
+++ Assignment2/ProblemSolving/116A/Program.cs
@@ -0,0 +1,16 @@
+﻿//Solution of the codeforces problem 116A - Tram
+int n = Convert.ToInt32(Console.ReadLine());
+int maxPassenger = 0;
+int passengerCount = 0;
+int[] stationPassenger;
+for(int i = 0; i < n; i++)
+{
+    stationPassenger = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    passengerCount -= stationPassenger[0];
+    passengerCount += stationPassenger[1];
+    if(passengerCount > maxPassenger)
+    {
+        maxPassenger = passengerCount;
+    }
+}
+Console.WriteLine(maxPassenger);
