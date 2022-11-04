033f0af24ee852042a55b6bc0d968378caab52c4
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-05 00:32:46 +0600

    Codeforces-1335A - Candies and Two Sisters Solved

---
+++ Assignment2/ProblemSolving/1335A/Program.cs
@@ -0,0 +1,7 @@
+﻿// Solution of the codeforces problem 1335A - Candies and Two Sisters
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(n%2==0?n/2-1:n/2);
+}
