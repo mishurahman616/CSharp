403a381184a325d63495df933f3563f61beb7d70
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-04 13:16:27 +0600

    Codeforces-996A - Hit the Lottery Solved

---
+++ Assignment2/ProblemSolving/ProblemSolving/Program.cs
@@ -0,0 +1,14 @@
+﻿//Solution of the codeforces problem 996A - Hit the Lottery
+    int n = int.Parse(Console.ReadLine());
+    int bill = 0;
+    bill += n / 100;
+    n %= 100;
+    bill += n / 20;
+    n %= 20;
+    bill += n / 10;
+    n %= 10;
+    bill += n / 5;
+    n %= 5;
+    bill += n;
+
+    Console.WriteLine(bill);
