5d859d2625ded0d6a17cac8e2665512e7b803a2f
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-05 17:49:58 +0600

    Codeforces-1367A-Short Substrings-Solved

---
+++ Assignment2/ProblemSolving/1367A/Program.cs
@@ -0,0 +1,12 @@
+﻿// Solution of the codefoces problem 1367A-Short Substrings
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    string b = Console.ReadLine();
+    Console.Write(b[0]);
+    for(int j = 1; j < b.Length-1; j+=2)
+    {
+        Console.Write(b[j]);
+    }
+    Console.WriteLine(b[b.Length-1]);
+}
