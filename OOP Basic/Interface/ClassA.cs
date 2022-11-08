2e515d4927ad2e3b49e3de8b6c19530b46617e6b
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-08 17:48:21 +0600

    interface

---
+++ OOP Basic/Interface/ClassA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class ClassA:IInterface
+    {
+        public void MethodA() => Console.WriteLine("Mehtod A");
+
+        public void InterfaceImplementation()
+        {
+            Console.WriteLine("From method Implementation");
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ OOP Basic/Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Interface;
+
+ClassC c = new ClassC();
+IInterface ca = new ClassA();
+c.methodA();
+
+ca.DefaultImplementation();
+ca.InterfaceImplementation();
