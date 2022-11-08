30ba95f84126e8aeecd9a5abd57dc12bbd3d2481
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-09 01:54:56 +0600

    Update Program.cs

--- Assignment3/Task1/Program.cs
+++ Assignment3/Task1/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Task1;
-
-Product product = new Product();
-product.Name = "Mishu";
-Console.WriteLine(product.Name);
+Console.WriteLine("Hello, World!");
