74c5572e523ad4a8a5ba59a3febb46857443fde6
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-14 01:06:00 +0600

    Codeforces-144A-Arrival of the General-Solved

---
+++ Assignment2/ProblemSolving/144A/Program.cs
@@ -0,0 +1,28 @@
+﻿// Solution of the codefoces problem 144A-Arrival of the General
+int n = Convert.ToInt32(Console.ReadLine());
+var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int max = int.MinValue;
+int min = int.MaxValue;
+int maxIndex = 0;
+int minIndex = 0;
+for(int i=0; i<n; i++)
+{
+    if (arr[i] > max)
+    {
+        maxIndex = i;
+        max= arr[i];
+    }
+    if (arr[i] <= min)
+    {
+        minIndex = i;
+        min= arr[i];
+    }
+}
+if (maxIndex < minIndex)
+{
+    Console.WriteLine(maxIndex + (n - 1 - minIndex));
+}
+else
+{
+    Console.WriteLine(maxIndex +n-2 - minIndex);
+}
