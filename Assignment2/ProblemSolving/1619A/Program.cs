153b22968ee1797502181528061358d1c8b1e3f0
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-14 23:28:22 +0600

    Codeforces-1619A-Square String-Solved

---
+++ Assignment2/ProblemSolving/1619A/Program.cs
@@ -0,0 +1,31 @@
+﻿// Solution of the codefoces problem 1619A- Square Stirng
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    string s = Console.ReadLine();
+    if (s.Length % 2 == 0)
+    {
+        int mid = s.Length/ 2-1;
+        int last = s.Length-1;
+        bool isSquare = true;
+        for(int j=0; j<s.Length/2; j++)
+        {
+            if (s[mid--] != s[last--])
+            {
+                Console.WriteLine("NO");
+                isSquare = false;
+                break;
+            }
+
+        }
+        if (isSquare)
+        {
+            Console.WriteLine("Yes");
+        }
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+
