505636842c7bb5bb78f073f67162758f86db4ead
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-14 22:48:34 +0600

    Codeforces-148A-Insomnia Cure - Solved

---
+++ Assignment2/ProblemSolving/148A/Program.cs
@@ -0,0 +1,27 @@
+﻿// Solution of the codefoces problem 148A-Insomnia cure
+
+int k0 = Convert.ToInt32(Console.ReadLine());
+int l0 = Convert.ToInt32(Console.ReadLine());
+int m0 = Convert.ToInt32(Console.ReadLine());
+int n0 = Convert.ToInt32(Console.ReadLine());
+int d = Convert.ToInt32(Console.ReadLine());
+int[] klmn= new int[] {k0, l0, m0, n0};
+bool[] arr = new bool[d+1];
+int counter = 0;
+for(int i = 0; i < 4; i++)
+{
+        for(int j = klmn[i]; j<=d; j += klmn[i])
+        {
+            arr[j]= true;
+        }
+}
+for(int i = 1; i<=d; i++)
+{
+    if (arr[i]==true)
+    {
+        counter++;
+
+    }
+}
+
+Console.WriteLine(counter);
