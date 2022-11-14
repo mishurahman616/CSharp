2c4217061fd7277b12a846d4e67561d7d1cda06e
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-15 00:49:59 +0600

    Codeforces-1621A-Stable Arrangement of Rooks

---
+++ Assignment2/ProblemSolving/1621A/Program.cs
@@ -0,0 +1,31 @@
+﻿//Solution of the codeforces problem 1621A-Stable Arrangement of Rooks
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    var x = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int n = x[0], k = x[1];
+    if (Math.Ceiling(n / 2.0) < k)
+    {
+        Console.WriteLine(-1);
+    }
+    else
+    {
+        int RPlace = 0;
+        int RPlaced = 0;
+        for(int row=0; row<n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (RPlace == row && RPlace == col && RPlaced<k)
+                {
+                    Console.Write("R");
+                    RPlace += 2;
+                    RPlaced++;
+                }else
+                Console.Write('.');
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
