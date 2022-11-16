091d4dda1fd1093e58352e4feae53bda6f315503
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 17:53:19 +0600

    Codeforces-266A-Stones on the Table-Solved

---
+++ Assignment2/ProblemSolving/266A/Program.cs
@@ -0,0 +1,12 @@
+﻿// Solution to the codeforces problem 266A-Stones on the Table
+int n = Convert.ToInt32(Console.ReadLine());
+string s = Console.ReadLine();
+int counter = 0;
+for(int i = 1; i < n; i++)
+{
+    if (s[i] == s[i - 1])
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);
