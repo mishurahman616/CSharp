1329432c4c9145165426f6b84e63ca268c955b83
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 13:50:17 +0600

    Codeforces-1623-A-Robot Cleaner Solved

---
+++ Assignment2/ProblemSolving/1623A/Program.cs
@@ -0,0 +1,26 @@
+﻿//Solution of the codefoces problem 1623A-Robot Cleaner
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    var data = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+    int timer = 0;
+    int robotX = data[2], robotY = data[3], dirtyX = data[4], dirtyY = data[5];
+    int xIncrement = 1, yIncrement = 1;
+    while(!(robotX==dirtyX || robotY == dirtyY))
+    {
+        if (robotX == data[0] && robotY == data[1])
+        {
+            xIncrement = -1; yIncrement = -1;
+        }else if (robotX == data[0])
+        {
+            xIncrement = -1;
+        }else if (robotY == data[1])
+        {
+            yIncrement = -1;
+        }
+        robotX += xIncrement;
+        robotY += yIncrement;
+        timer++;
+    }
+    Console.WriteLine(timer);
+}
