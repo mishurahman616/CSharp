ca9b51e4efa30d9871f0e49813e78d97340e6251
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 14:44:30 +0600

    Codefoces-problem 200B-Drinks-Solved

--- Assignment2/ProblemSolving/200B/Program.cs
+++ Assignment2/ProblemSolving/200B/Program.cs
@@ -1,4 +1,4 @@
-﻿// Solution of the codefoces problem 200B-Drinks
+﻿// Solution to the codefoces problem 200B-Drinks
 int n = Convert.ToInt32(Console.ReadLine());
 var data = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
 int sum = 0;
