73d18bdde2b864bbdc86f818e4a9bd9265db9f88
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 14:04:24 +0600

    Codeforces-200B-Drinks Solved

---
+++ Assignment2/ProblemSolving/200B/Program.cs
@@ -0,0 +1,9 @@
+﻿// Solution of the codefoces problem 200B-Drinks
+int n = Convert.ToInt32(Console.ReadLine());
+var data = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+int sum = 0;
+foreach(var item in data)
+{
+    sum += item;
+}
+Console.WriteLine(((sum*1.0)/n).ToString("0.000000000000"));
