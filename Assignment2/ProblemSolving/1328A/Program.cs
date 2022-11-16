4473bad02799e3fc1ad603978866d816cf206925
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 14:48:22 +0600

    Codefoces-1328A - Divisibility Problem-Sovled

--- Assignment2/ProblemSolving/1328A/Program.cs
+++ Assignment2/ProblemSolving/1328A/Program.cs
@@ -1,4 +1,4 @@
-﻿// Solution of the codeforces problem 1328A - Divisibility Problem
+﻿// Solution to the codeforces problem 1328A - Divisibility Problem
 int t = Convert.ToInt32(Console.ReadLine());
 for(int i = 0; i < t; i++)
 {
