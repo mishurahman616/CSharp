28392ffd64bfae5859df86d20825c0d6f9507f43
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 14:23:34 +0600

    Codefoces-263A-Beautiful Matrix Solved

---
+++ Assignment2/ProblemSolving/263A/Program.cs
@@ -0,0 +1,17 @@
+﻿// Solution of the codefoces problem 263A-Beutiful Matrix
+int[][] matrix = new int[5][];
+for(int i=0; i<5; i++)
+{
+    matrix[i] = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+}
+
+for(int i=0; i<5; i++)
+{
+    for(int j=0; j<5; j++)
+    {
+        if (matrix[i][j] == 1) {
+            Console.WriteLine(Math.Abs(2-i)+Math.Abs(2-j));
+            break;
+        }
+    }
+}
