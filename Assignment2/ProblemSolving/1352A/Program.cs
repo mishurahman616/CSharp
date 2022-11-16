41d141ba184812e5a287e10b7e06393f817fc7e5
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-16 14:49:06 +0600

    Codefoces-1352A - Sum of Round Numbers-Solved

--- Assignment2/ProblemSolving/1352A/Program.cs
+++ Assignment2/ProblemSolving/1352A/Program.cs
@@ -1,4 +1,4 @@
-﻿// Solution of the codeforces problem 1352A - Sum of Round Numbers
+﻿// Solution to the codeforces problem 1352A - Sum of Round Numbers
 int t = Convert.ToInt32(Console.ReadLine());
 for(int i = 0; i < t; i++)
 {
