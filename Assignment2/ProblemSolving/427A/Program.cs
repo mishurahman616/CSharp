d1877fd75201e04b34a2a4c0d55966304738fae0
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-18 03:00:41 +0600

    Codeforces-427A-Police Recruits-Solved

---
+++ Assignment2/ProblemSolving/427A/Program.cs
@@ -0,0 +1,26 @@
+﻿// Solution to the codeforces problem 427A-Police Recruits
+int n = Convert.ToInt32(Console.ReadLine());
+var data = Console.ReadLine().Split().Select(x => Convert.ToInt32(x)).ToArray();
+
+uint freePolice = 0;
+int unTreated = 0;
+foreach(var item in data)
+{
+    if (item == -1)
+    {
+        if (freePolice == 0)
+        {
+            unTreated++;
+        }
+        else
+        {
+            freePolice--;
+        }
+    }
+    else
+    {
+        freePolice += (uint) item;
+    }
+}
+
+Console.WriteLine(unTreated);
