4fc2d3d18c3acfce9a8a01f1ce2bf446c31d918c
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-17 18:10:54 +0600

    Codeforces-432A-Choosing Teams-Solved

---
+++ Assignment2/ProblemSolving/432A/Program.cs
@@ -0,0 +1,13 @@
+﻿// Solution to the codeforces problem 432A-Choosing Teams
+var nk = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+var y = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+int maxLimit = 5 - nk[1];
+int counter = 0;
+foreach(var item in y)
+{
+    if(item<=maxLimit)
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter/3);
