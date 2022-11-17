2a3631336f6652904a4fb3ca36a12bdb1b660cd3
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-17 14:54:09 +0600

    Codeforces-472A-Design Tutorial: Learn from Math-Solved

---
+++ Assignment2/ProblemSolving/472A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Solution to the codeforcs problem 472A-Design Tutorial: Learn from Math
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{4} {n - 4}");
+}
+else
+{
+    Console.WriteLine($"{9} {n-9}");
+}
