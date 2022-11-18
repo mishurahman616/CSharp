6372e18f725efccb30a96a6546decf71a864f0ad
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-18 15:53:08 +0600

    Codeforces-271A-Beautiful Year-Solved

---
+++ Assignment2/ProblemSolving/271A/Program.cs
@@ -0,0 +1,52 @@
+﻿//Solution to the codeforces problem 271A- Beautiful Year
+var data = Console.ReadLine();
+int onePlace = data[3] - '0';
+int tenPlace = data[2] - '0';
+int hundredPlace = data[1] - '0';
+int thousandPlace = data[0] - '0';
+bool change = false;
+while (true)
+{
+    Console.WriteLine($"{thousandPlace}{hundredPlace}{tenPlace}{onePlace}");
+    if (onePlace == 9)
+    {
+        onePlace= 0;
+        if (tenPlace == 9) 
+        { 
+            change = true; 
+        }
+        else 
+        {
+            tenPlace++;
+            change = false;
+        }
+
+    }
+    else
+    {
+        onePlace++;
+    }
+    if(tenPlace== 9 && change)
+    {
+        tenPlace= 0;
+        if(hundredPlace== 9) 
+        { 
+            change = true;
+        }
+        else
+        {
+            hundredPlace++;
+            change = false;
+        }
+    } 
+    if(hundredPlace== 9 && change)
+    {
+        hundredPlace= 0;
+        thousandPlace+= 1;
+    }
+    if(onePlace != tenPlace && onePlace != hundredPlace && onePlace != thousandPlace && tenPlace != hundredPlace && tenPlace != thousandPlace && hundredPlace != thousandPlace)
+    {
+        break;
+    }
+}
+Console.WriteLine($"{thousandPlace}{hundredPlace}{tenPlace}{onePlace}");
