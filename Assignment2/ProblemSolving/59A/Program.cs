2a2e0a0a1e8f3678f682fe81ff644f99b42afaf1
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-19 00:58:18 +0600

    Codeforces-59A-Word-Solved

---
+++ Assignment2/ProblemSolving/59A/Program.cs
@@ -0,0 +1,19 @@
+﻿//Solution to the codeforces problem 469A-Word
+string s = Console.ReadLine();
+int counter = 0;
+for(int i=0; i<s.Length; i++)
+{
+    if (s[i]>='a' && s[i] <= 'z')
+    {
+        counter++;
+    }
+}
+
+if(counter >= Math.Ceiling(s.Length / 2.0))
+{
+    Console.WriteLine(s.ToLower());
+}
+else
+{
+    Console.WriteLine(s.ToUpper());
+}
