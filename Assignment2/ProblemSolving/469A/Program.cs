3e2d1a09b22da8d80a78bec29efd3dd1a7973b26
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-19 00:38:19 +0600

    Codeforce-469A-I Wanna Be the Guy-Solved

--- Assignment2/ProblemSolving/469A/Program.cs
+++ Assignment2/ProblemSolving/469A/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Solution to the codeforces problem 469A-I Wanna Be the Guy
+int n = Convert.ToInt32(Console.ReadLine());
+var X = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+var Y = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int counter = X[0];
+bool[] solved = new bool[n+1];
+for(int i=1; i <= X[0]; i++)
+{
+    solved[X[i]] = true;
+}
+
+for(int i=1; i <= Y[0]; i++)
+{
+    if (solved[Y[i]] == false)
+    {
+        counter++;
+    }
+}
+
+if(counter == n)
+{
+    Console.WriteLine("I become the guy.");
+}
+else
+{
+    Console.WriteLine("Oh, my keyboard!");
+}
