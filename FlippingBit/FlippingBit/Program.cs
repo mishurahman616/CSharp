db86e65ed3d9d85ba95028cc3da8f068e5efc3bc
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-22 19:58:15 +0600

    Update Program.cs

--- FlippingBit/FlippingBit/Program.cs
+++ FlippingBit/FlippingBit/Program.cs
@@ -1,12 +1,16 @@
 ﻿
-
+// Flipping bit
 long a = 2147483647;
 int a2 = (int)a;
 string s = Convert.ToString(~a2, toBase:2);
 Console.WriteLine(s);
 Console.WriteLine(Convert.ToUInt32(s, 2));
+
+//String to char access
 string s2 = "abCd";
 Console.WriteLine(Char.ToLower(s2[2]) - 'a');
+
+//others
 int k = 100;
 int k1 = 200;
 List<int> l = new List<int>();
