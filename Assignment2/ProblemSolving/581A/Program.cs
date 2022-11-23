e46b327c7eb5fa82b279c5886f475f4f482958fc
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-23 20:05:58 +0600

    Codeforces-581A-Vasya the Hipster-Solved

---
+++ Assignment2/ProblemSolving/581A/Program.cs
@@ -0,0 +1,5 @@
+﻿// Solution to the codeforces problem - 581A-Vasya the Hipster
+var x = Console.ReadLine().Trim().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+int max = x.Max();
+int min = x.Min();
+Console.WriteLine($"{min} {(max-min)/2}");
