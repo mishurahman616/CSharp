8e69fa064b6ca3fa0215635a1665f14529c2f84f
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-23 19:14:28 +0600

    Codeforces-50A-50A-Domino piling-Solved

---
+++ Assignment2/ProblemSolving/50A/Program.cs
@@ -0,0 +1,3 @@
+﻿// Solution to the codeforces problem - 50A-Domino piling
+var xy = Console.ReadLine().Trim().Split().ToList().Select(x => Convert.ToInt32(x)).ToList();
+Console.WriteLine(xy[0] * xy[1]/2);
