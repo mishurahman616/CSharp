af3351953eaebd03831213628584e45829f160c7
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-23 19:50:59 +0600

    Codeforces-486A-Calculating function-Solved

---
+++ Assignment2/ProblemSolving/486A/Program.cs
@@ -0,0 +1,11 @@
+﻿// Solution to the codeforces problem - 486A-Calculating function
+long x = Convert.ToInt64(Console.ReadLine().Trim());
+long halfX = x>>1;
+if (x % 2 == 0)
+{
+    Console.WriteLine(halfX);
+}
+else
+{
+    Console.WriteLine((halfX-x));
+}
