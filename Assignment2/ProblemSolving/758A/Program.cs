4a92f53e6ed26cee4d8f619260edfc68b0eaf63c
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-24 17:46:28 +0600

    Codeforces-758A - Holiday Of Equality-Solved

---
+++ Assignment2/ProblemSolving/758A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Solution to the codeforces problem - 758A-Holiday Of Equality
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+var treasury = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).ToList();
+long sum = 0;
+int max = treasury.Max();
+for(int i=0; i<treasury.Count; i++)
+{
+    sum+= max-treasury[i];
+}
+Console.WriteLine(sum);
