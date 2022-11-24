21b3b5971c58f87414dc94ae7877a405ff6a7a6c
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-24 17:33:59 +0600

    Codeforces-785A - Anton and Polyhedrons-Solved

---
+++ Assignment2/ProblemSolving/785A/Program.cs
@@ -0,0 +1,30 @@
+﻿//Solution to the codeforces problem - 785A-Anton and Polyhedrons
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+int sum = 0;
+for (int i=0; i<n; i++)
+{
+    string s = Console.ReadLine();
+  
+    switch (s)
+    {
+        case "Tetrahedron":
+            sum += 4;
+            break;
+        case "Cube":
+            sum += 6;
+            break;
+        case "Octahedron":
+            sum += 8;
+            break;
+        case "Dodecahedron":
+            sum += 12;
+            break;
+        case "Icosahedron":
+            sum += 20;
+            break;
+
+    }
+    
+}
+Console.WriteLine(sum);
