baba6963c5602d6e8e30ae3cbf2788969abc1a24
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 17:22:28 +0600

    Codeforces - 723A-he New Year: Meeting Friends-Solved

---
+++ Assignment2/ProblemSolving/723A/Program.cs
@@ -0,0 +1,4 @@
+﻿// Solution to the codeforces problem - 723A-he New Year: Meeting Friends
+var x = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).ToList();
+x.Sort();
+Console.WriteLine($"{x[2] - x[0]}"); //
