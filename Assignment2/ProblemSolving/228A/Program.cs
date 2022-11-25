9847e12bcba3594cb93b3df7f326eda070572bbc
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 10:47:09 +0600

    Codeforces-228A-Is your horseshoe on the other hoof?-Solved

---
+++ Assignment2/ProblemSolving/228A/Program.cs
@@ -0,0 +1,4 @@
+﻿// Solution to the codeforces problem - 228A-Is your horseshoe on the other hoof?
+
+HashSet<string> horsshoe = Console.ReadLine().Trim().Split().ToHashSet();
+Console.WriteLine(4-horsshoe.Count);
