ac62a4d14c81e3904338d43ffbed2876fbdb13e6
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 10:41:37 +0600

    Codeforces-122A-Lucky Division-solved

---
+++ Assignment2/ProblemSolving/122A/Program.cs
@@ -0,0 +1,28 @@
+﻿// Solution to the codeforces problem - 122A-Lucky Division
+
+string s = Console.ReadLine();
+int n = Convert.ToInt32(s);
+bool lucky=true;
+ for(int i=0; i<s.Length; i++)
+{
+    if (!(s[i]=='4' || s[i]=='7'))
+    {
+        lucky= false;
+        break;
+    }
+}
+if (lucky)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    if (n % 4 == 0 || n % 7 == 0 || n % 47 == 0 || n % 74 == 0)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
