7d78f3ad62d076371cf99dca5df23b80d664c2a3
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 18:00:47 +0600

    Codeforces-750A-New Year and Hurry-Solved

---
+++ Assignment2/ProblemSolving/750A/Program.cs
@@ -0,0 +1,13 @@
+﻿// Solution to the codeforces problem - 750A-New Year and Hurry
+var data = Console.ReadLine().Trim().Split().Select(s => Convert.ToInt32(s)).ToList();
+int maxTime = 240 - data[1];
+int count = 0;
+int time = 5;
+int n = data[0];
+while (time <= maxTime && n-->0)
+{
+    count++;
+    time += (count + 1) * 5;
+
+}
+Console.WriteLine(count);
