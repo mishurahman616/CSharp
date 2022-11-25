bf805fb93a1cc24fa7b65365140edd26f9871713
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 17:07:14 +0600

    Codeforces-705A-Hulk-Solved

---
+++ Assignment2/ProblemSolving/705A/Program.cs
@@ -0,0 +1,19 @@
+﻿// Solution to the codeforces problem - 705A-Hulk
+int n = Convert.ToInt32(Console.ReadLine());
+bool love = true;
+Console.Write("I hate ");
+for (int i = 2; i <= n; i++)
+{
+    if (love)
+    {
+        Console.Write("that I love ");
+        love= false;
+    }
+    else
+    {
+        Console.Write("that I hate ");
+        love= true;
+    }
+   
+}
+Console.Write("it");
