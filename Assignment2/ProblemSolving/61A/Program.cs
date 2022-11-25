8492016ac46505f3065244e67d742bbfeeca6f64
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 16:33:44 +0600

    Codeforces-61A-Ultra-Fast Mathematician-Solved

---
+++ Assignment2/ProblemSolving/61A/Program.cs
@@ -0,0 +1,15 @@
+﻿// Solution to the codeforces problem - 61A-Ultra-Fast Mathematician
+
+string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+for(int i=0; i<s1.Length; i++)
+{
+    if (s1[i] == s2[i])
+    {
+        Console.Write("0");
+    }
+    else
+    {
+        Console.Write("1");
+    }
+}
