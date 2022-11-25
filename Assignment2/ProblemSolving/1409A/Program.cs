06141ca344e288663f2842da6d59df327fe1f91f
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-26 01:24:23 +0600

    Codeforces-1409A-Yet Another Two Integers Problem-Solved

---
+++ Assignment2/ProblemSolving/1409A/Program.cs
@@ -0,0 +1,9 @@
+﻿// Solution to the codeforces problem - 1409A-Yet Another Two Integers Problem
+
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < n; i++)
+{
+    var data = Console.ReadLine().Trim().Split().Select(s => Convert.ToInt32(s)).ToList();
+    var dif = Math.Abs(data[0] - data[1]);
+    Console.WriteLine(Math.Ceiling(dif / 10.0));
+}
