ff9a27fd2f92145aa811a7321e7a6606d02b1d40
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-25 17:37:31 +0600

    Codeforces - 732A-Buy a Shovel-Solved

---
+++ Assignment2/ProblemSolving/732A/Program.cs
@@ -0,0 +1,12 @@
+﻿// Solution to the codeforces problem - 732A-Buy a Shovel
+
+var x = Console.ReadLine().Trim().Split().Select(s=>Convert.ToInt32(s)).ToList();
+int counter = 1;
+int k = x[0];
+while (!(k % 10==0 || (k - x[1]) % 10 == 0))
+{
+    k += x[0];
+    counter++;
+}
+
+Console.WriteLine(counter);
