3a31daf384e873dbbc178a7a77d01fa8f6f2fd7d
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-26 14:56:17 +0600

    Codeforces-158A-Next Round-Solved

---
+++ Assignment2/ProblemSolving/158A/Program.cs
@@ -0,0 +1,13 @@
+﻿// Solution to the codeforces problem - 158A-Next Round
+var nk = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+var data = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).ToArray();
+int kth = Convert.ToInt32(data[nk[1]-1]);
+int counter = 0;
+foreach(var item in data)
+{
+    if(item>=kth && item > 0)
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);
