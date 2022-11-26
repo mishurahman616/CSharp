02376d83263f4afeb68dc3c11352789026ed7ab1
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-26 15:22:30 +0600

    Codeforces-1A-Theatre Square-Solved

---
+++ Assignment2/ProblemSolving/1A/Program.cs
@@ -0,0 +1,8 @@
+﻿//Solution to the codefores problem - 1A-Theatre Square
+
+var data = Array.ConvertAll(Console.ReadLine().Trim().Split(), long.Parse);
+
+long x = (long)Math.Ceiling(data[0] * 1.0 / data[2]);
+long y = (long)Math.Ceiling(data[1] * 1.0 / data[2]);
+Console.WriteLine((long)(x * y));
+
