4afe437cf8b4887a6e007250e7566534c6ea4b9a
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-26 13:13:00 +0600

    Codeforces-155A-I_love_%username%-Solved

---
+++ Assignment2/ProblemSolving/155A/Program.cs
@@ -0,0 +1,21 @@
+﻿// Solution to the codeforces problem 155A-I_love_%username%
+int n = Convert.ToInt32(Console.ReadLine());
+
+var data = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).ToList();
+int max = data[0];
+int min = data[0];
+int amazing = 0;
+for (int i=1; i<n; i++)
+{
+    if (data[i] > max)
+    {
+        max = data[i];
+        amazing++;
+    }else if (data[i] < min)
+    {
+        min = data[i];
+        amazing++;
+    }
+
+}
+Console.WriteLine(amazing);
