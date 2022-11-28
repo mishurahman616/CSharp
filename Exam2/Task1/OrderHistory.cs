ee9228cbace88e0e2f00fb62ad2bda620026f428
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-28 22:21:06 +0600

    task 1

--- Exam2/Task1/OrderHistory.cs
+++ Exam2/Task1/OrderHistory.cs
@@ -8,27 +8,30 @@ namespace Task1
 {
     public class OrderHistory
     {
-        (string name, int age)[] persons = new (string name, int age)[]
+
+
+        dynamic persons = new[] 
+        { 
+            new {name="jalaluddin", age=41}, 
+            new {name="tareq", age=33}, 
+            new {name="hasan", age=52}, 
+            new {name="rashed", age=21}, 
+            new{name="monir", age=45} 
+        };
+        dynamic orders = new[]
         {
-            ("jalaluddin", 41),
-            ("tareq", 33),
-            ("hasan", 52),
-            ("rashed", 21),
-            ("monir", 45)
+            new{name="monir", amount=300.5},
+            new{name="rashed", amount=20.5},
+            new{name="tareq", amount=29.9},
+            new{name="hasan", amount=17.7},
+            new{name="jalaluddin", amount=30.2},
+
         };
 
-        (string name, double amount)[] orders = new (string name, double amount)[]
-        {
-            ("monir", 300.5),
-            ("rashed", 20.5),
-            ("tareq", 29.9),
-            ("hasan", 17.7),
-            ("jalaluddin", 30.2)
-        };
 
-        public List<(string, double)> GetOrders()
+        public object GetOrders()
         {
-            var result = new List<(string, double)>();
+            var result = new List< object >();
 
             foreach (var person in persons)
             {
@@ -36,7 +39,9 @@ namespace Task1
                 {
                     if(person.name == order.name)
                     {
-                        result.Add((person.name, order.amount));
+
+                        result.Add(new {person.name, order.amount});
+
                     }
                 }
             }
