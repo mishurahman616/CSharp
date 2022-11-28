68addc5c44c4fa402c4856c7f561949cd5bdbd5a
Mishu Rahman <mishu.cse616@gmail.com>
2022-11-28 22:24:44 +0600

    Task 2

--- Exam2/Task2/Form.cs
+++ Exam2/Task2/Form.cs
@@ -6,18 +6,12 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    public class Form
+    public partial class Form
     {
         int _value1;
         string _value2;
         double _value3;
         bool _value4;
         DateTime _value5;
-
-        public int Value1 { get { return _value1; } set { _value1 = value; } }
-        public string Value2 { get { return _value2; } set { _value2 = value; } }
-        public double Value3 { get { return _value3; } set { _value3 = value; } }
-        public bool Value4 { get { return _value4; } set { _value4 = value; } }
-        public DateTime Value5 { get { return _value5; } set { _value5 = value; } }
     }
 }
---
+++ Exam2/Task2/FormProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public partial class Form
+    {
+        public int Value1 { get { return _value1; } set { _value1 = value; } }
+        public string Value2 { get { return _value2; } set { _value2 = value; } }
+        public double Value3 { get { return _value3; } set { _value3 = value; } }
+        public bool Value4 { get { return _value4; } set { _value4 = value; } }
+        public DateTime Value5 { get { return _value5; } set { _value5 = value; } }
+    }
+}
