f7ef80b47b7a5680644a7f71e423d60cefe53857
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-04 03:13:17 +0600

    Db Connection

---
+++ ADO/ADO/DataUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace ADO
+{
+    class DataUtility
+    {
+        readonly SqlConnection _connection;
+        private SqlCommand _sqlCommand;
+
+        public DataUtility()
+        {
+            _connection = new SqlConnection("data source=.; database=CSharp; integrated security=SSPI");
+        }
+        public void GiveSqlCommand(string sqlCommand)
+        {
+            _sqlCommand = new SqlCommand(sqlCommand);
+        }
+
+        
+    }
+}
---
+++ ADO/ADO/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+
+SqlConnection con = null;
+
+try
+{
+    // Creating Connection  
+    string ConnectionString = "Data Source = ASUS\\SQLEXPRESS; Initial Catalog=CSharp; Integrated Security = True";
+    con = new SqlConnection(ConnectionString);
+    con.Open();
+
+
+    Console.WriteLine("Connection Established Successfully");
+    SqlCommand sqlCommand = new SqlCommand("Select * from Courses", con);
+    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+    while (sqlDataReader.Read())
+    {
+        Console.WriteLine($"{sqlDataReader["title"].ToString().Trim()} {sqlDataReader[2]} {sqlDataReader[5],-20}");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("OOPs, something went wrong.\n" + e);
+}
+finally
+{   // Closing the connection  
+    con.Close();
+}
+
+int num = 302;
+string numStr = String.Format("Number {0, 0:D4}", num);
+Console.WriteLine(numStr);
+
+// Decimal formatting  
+decimal money = 99.95m;
+string moneyStr = String.Format("Money {0, 4:C5}", money);
+Console.WriteLine(moneyStr);
+
+// DateTime formatting  
+DateTime now = DateTime.Now;
+string dtStr = String.Format("{0:d} at {0:t}", now);
+Console.WriteLine(dtStr);
