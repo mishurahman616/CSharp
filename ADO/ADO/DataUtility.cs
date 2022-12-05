bfb44c8c431be146bb86c51f905e57cd0d641934
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-05 10:43:22 +0600

    Data Insert, Select query

--- ADO/ADO/DataUtility.cs
+++ ADO/ADO/DataUtility.cs
@@ -10,18 +10,65 @@ namespace ADO
 {
     class DataUtility
     {
-        readonly SqlConnection _connection;
+
+        private string _connectionString;
         private SqlCommand _sqlCommand;
 
         public DataUtility()
         {
-            _connection = new SqlConnection("data source=.; database=CSharp; integrated security=SSPI");
+            _connectionString = @"Server = ASUS\SQLEXPRESS; Database = CSharp; Trusted_Connection = True";
         }
-        public void GiveSqlCommand(string sqlCommand)
+        public DataUtility(string connectionString)
         {
-            _sqlCommand = new SqlCommand(sqlCommand);
+            if (connectionString != null)
+            {
+                _connectionString = connectionString;
+            }
+            else
+            {
+                _connectionString = @"Server = ASUS\SQLEXPRESS; Database = CSharp; Trusted_Connection = True";
+            }
+                
         }
-
+        public int ExecuteCommand(string commandText, IList<SqlParameter> parameters)
+        {
+            using SqlCommand cmd = GetCommand(commandText, parameters);
+            return cmd.ExecuteNonQuery();
+        }
+        public object ExecuteScalar(string commandText, IList<SqlParameter> parameters)
+        {
+            using SqlCommand cmd = GetCommand(commandText, parameters);
+            return cmd.ExecuteScalar();
+        }
+        public List<Dictionary<string, object>> ExecuteQuery(string query, IList<SqlParameter> parameters)
+        {
+            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+            using SqlCommand cmd = GetCommand(query, parameters);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                for(int i=0; i<reader.FieldCount; i++)
+                {
+                    dict.Add(reader.GetName(i), reader.GetValue(i));
+                }
+                result.Add(dict);
+            }
+            return result;
+        }
+        private SqlCommand GetCommand(string commandText, IList<SqlParameter> parameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = commandText;
+            if(connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            cmd.Parameters.AddRange(parameters.ToArray());
+            return cmd;
+        }
         
     }
 }
--- ADO/ADO/Program.cs
+++ ADO/ADO/Program.cs
@@ -1,29 +1,57 @@
-﻿using System.Data.SqlClient;
+﻿using ADO;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 
-SqlConnection con = null;
+//SqlConnection con = null;
 
-try
+//try
+//{
+//    // Creating Connection  
+//    string ConnectionString = "Data Source = ASUS\\SQLEXPRESS; Initial Catalog=CSharp; Integrated Security = True";
+//    con = new SqlConnection(ConnectionString);
+//    con.Open();
+
+
+//    Console.WriteLine("Connection Established Successfully");
+//    SqlCommand sqlCommand = new SqlCommand("Select * from Courses", con);
+//    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+//    while (sqlDataReader.Read())
+//    {
+//        Console.WriteLine($"{sqlDataReader["title"].ToString().Trim()} {sqlDataReader[2]} {sqlDataReader[5],-20}");
+//    }
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine("OOPs, something went wrong.\n" + e);
+//}
+//finally
+//{   // Closing the connection  
+//    con.Close();
+//}
+
+//Console.Write("Enter Course Title: ");
+//string title = Console.ReadLine();
+//Console.Write("\nEnter Course Fees: ");
+//double fees = Double.Parse(Console.ReadLine());
+//Console.Write("\nEnter Course Class Start Date: ");
+//DateTime classStart = DateTime.Parse(Console.ReadLine());
+//Console.Write("\nIs Course Open now? ");
+//bool isOpen = bool.Parse(Console.ReadLine());
+
+//string insertQuery = "INSERT INTO Courses(title, fees, classStart, isopen, courseid) VALUES(@title, @fees, @classStart, @isOpen, @courseid);";
+List<SqlParameter> parameters= new List<SqlParameter>();
+//parameters.Add(new SqlParameter("title", title));
+//parameters.Add(new SqlParameter("fees", fees));
+//parameters.Add(new SqlParameter("classStart", classStart));
+//parameters.Add(new SqlParameter("isOpen", isOpen));
+//parameters.Add(new SqlParameter("courseid", Guid.NewGuid()));
+
+DataUtility datautility = new DataUtility();
+//dataUtility.ExecuteCommand(insertQuery, parameters);
+
+string selectQuery = "SELECT TOP(10) * from Courses";
+   List<Dictionary<string, object>> result = datautility.ExecuteQuery(selectQuery, parameters);
+   foreach(var rs in result)
 {
-    // Creating Connection  
-    string ConnectionString = "Data Source = ASUS\\SQLEXPRESS; Initial Catalog=CSharp; Integrated Security = True";
-    con = new SqlConnection(ConnectionString);
-    con.Open();
-
-
-    Console.WriteLine("Connection Established Successfully");
-    SqlCommand sqlCommand = new SqlCommand("Select * from Courses", con);
-    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-    while (sqlDataReader.Read())
-    {
-        Console.WriteLine($"{sqlDataReader["title"].ToString().Trim()} {sqlDataReader[2]} {sqlDataReader[5],-20}");
-    }
+    Console.WriteLine(rs["title"]);
 }
-catch (Exception e)
-{
-    Console.WriteLine("OOPs, something went wrong.\n" + e);
-}
-finally
-{   // Closing the connection  
-    con.Close();
-}
-
