8bb97df09035fc71de7ef4dc1091b5f694dcc8ef
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-15 23:45:58 +0600

    Codeforces  - 1754A-Technical Support-Solved

--- Assignment2/ProblemSolving/1754A/Program.cs
+++ Assignment2/ProblemSolving/1754A/Program.cs
@@ -1,4 +1,4 @@
-﻿//Solution to the codefores problem - 1754A-Technical Support
+﻿//Solution to the codeforces problem - 1754A-Technical Support
 int t = Convert.ToInt32(Console.ReadLine());
 for(int i = 0; i < t; i++)
 {
