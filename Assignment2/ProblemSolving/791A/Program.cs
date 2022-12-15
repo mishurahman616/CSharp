1c871bde57e24189444eb6db31ec483488e273a9
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-16 00:01:18 +0600

    Codeforces - 791A-Bear and Big Brother-Solved

---
+++ Assignment2/ProblemSolving/791A/Program.cs
@@ -0,0 +1,11 @@
+﻿// Solution to the codeforces problem - 791A-Bear and Big Brother
+using System.Diagnostics.Metrics;
+var lb = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int counter = 0;
+while (lb[1] >= lb[0])
+{
+    lb[0] *= 3;
+    lb[1] *= 2;
+    counter++;
+}
+Console.WriteLine(counter);
