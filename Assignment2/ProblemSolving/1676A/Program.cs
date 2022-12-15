7bdc20e3345815116e3937b8699377e0ac80adce
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-15 09:48:46 +0600

    Codeforces-1676A-Lucky?-Solved

---
+++ Assignment2/ProblemSolving/1676A/Program.cs
@@ -0,0 +1,23 @@
+﻿// Solution to the codefores problem - 1676A-Lucky?
+
+int n = Convert.ToInt32(Console.ReadLine());
+for(int j = 0; j < n; j++)
+{
+    string s = Console.ReadLine();
+    int sum1=0, sum2=0;
+    for(int i=0; i<3; i++)
+    {
+        sum1 += Convert.ToInt32(s[i]);
+        sum2 += Convert.ToInt32(s[i + 3]);
+    }
+
+    if (sum1 == sum2)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+
