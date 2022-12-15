1ed528ef092e162b692d57a92f1018b5a7fab012
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-15 09:33:19 +0600

    Codeforces-1754A-Technical Support-Solved

---
+++ Assignment2/ProblemSolving/1754A/Program.cs
@@ -0,0 +1,34 @@
+﻿//Solution to the codefores problem - 1754A-Technical Support
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+
+    int n = Convert.ToInt32(Console.ReadLine());
+    string sequence = Console.ReadLine();
+    int q = 0;
+    for(int j=0; j<n; j++)
+    {
+
+        if (sequence[j] == 'Q')
+        {
+            q++;
+        }
+        else
+        {
+            if (q > 0)
+            {
+                q--;
+            }
+        }
+
+    }
+    if(q<=0)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+
+}
