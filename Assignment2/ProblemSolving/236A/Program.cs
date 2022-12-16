0120efabcab31dd52aa68905f8751e16a3e5faaf
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-16 10:01:11 +0600

    Codeforces- 236A-Boy or Girl-Solved

---
+++ Assignment2/ProblemSolving/236A/Program.cs
@@ -0,0 +1,24 @@
+﻿// Solution to the codeforces problem - 236A-Boy or Girl
+
+string s = Console.ReadLine();
+bool[] letters = new bool[26];
+int distinct = 0;
+for(int i=0; i<s.Length; i++)
+{
+    letters[s[i] - 97] = true;
+}
+for(int i=0; i<letters.Length; i++)
+{
+    if (letters[i])
+    {
+        distinct++;
+    }
+}
+if (distinct %2 == 0)
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
+else
+{
+    Console.WriteLine("IGNORE HIM!");
+}
