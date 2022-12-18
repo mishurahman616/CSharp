c5e4fb3d7236d96ca684994e4cf351a33d061b0e
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-19 01:17:04 +0600

    Codeforces-110A-Nearly Lucky Number-Solved

---
+++ Assignment2/ProblemSolving/110A/Program.cs
@@ -0,0 +1,29 @@
+﻿// Solution to the codeforces problem - 110A-Nearly Lucky Number
+string s = Console.ReadLine();
+
+int i = 0, l = s.Length - 1;
+int counter = 0;
+while (i < l)
+{
+    if (s[i]=='4' || s[i] == '7')
+    {
+        counter++;
+    }
+    if ( s[l] == '4' || s[l] == '7')
+    {
+        counter++;
+    }
+    i++;
+    l--;
+}
+if(i==l && (s[i]=='4' || s[i] == '7'))
+{
+    counter++;
+}
+if (counter==4 || counter == 7)
+{
+    Console.WriteLine("YES");
+}else
+{
+    Console.WriteLine("NO");
+}
