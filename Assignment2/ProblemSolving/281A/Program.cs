fd16a6bf9a3f9a6f4afa0b25be70bd80203d8b1d
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 21:25:14 +0600

    Codeforces- 281A-Word Capitalization-Solved

---
+++ Assignment2/ProblemSolving/281A/Program.cs
@@ -0,0 +1,8 @@
+﻿// Solution to the codeforces problem - 281A-Word Capitalization
+using System.Text;
+
+string s=Console.ReadLine();
+
+StringBuilder stringBuilder=new StringBuilder(s);
+stringBuilder[0] = char.ToUpper(s[0]);
+Console.WriteLine(stringBuilder);
