7dc0fc96fc392aba0e0222eeb06ba536e618a114
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 18:47:30 +0600

    Codeforces- 4A-Watermelon-Solved

---
+++ Assignment2/ProblemSolving/4A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Solution to the codeforces problem - 4A-Watermelon
+int n = Convert.ToInt32(Console.ReadLine());
+if(n>2 && n % 2 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
