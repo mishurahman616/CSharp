bc8638ae730419526ece2950816ec0564a9fc29d
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 13:21:56 +0600

    Codeforces-546A-Soldier and Bananas-Solved

---
+++ Assignment2/ProblemSolving/546A/Program.cs
@@ -0,0 +1,6 @@
+﻿// Solution to the codeforces problem - 546A-Soldier and Bananas
+
+var data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int x = data[2] * (data[2] + 1) / 2 * data[0] - data[1];
+Console.WriteLine(x > 0 ? x : 0);
