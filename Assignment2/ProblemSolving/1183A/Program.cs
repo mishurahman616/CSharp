5666281b9ee42faef3297b131b91a567c00985f6
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-19 02:02:38 +0600

    Codeforces- 1183A-Nearest Interesting Number-Solved

---
+++ Assignment2/ProblemSolving/1183A/Program.cs
@@ -0,0 +1,23 @@
+﻿// Solution to the codeforces problem - 1183A-Nearest Interesting Number
+
+int n = Convert.ToInt32(Console.ReadLine());
+int x = n;
+int sum = 0;
+while (x > 0)
+{
+    sum += x % 10;
+    x /= 10;
+}
+
+while(sum%4 != 0)
+{
+    n = n + 1;
+     x = n;
+     sum = 0;
+    while (x > 0)
+    {
+        sum += x % 10;
+        x /= 10;
+    }
+}
+Console.WriteLine(n);
