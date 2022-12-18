77dfada6898262221063f6053020d423f01497aa
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-19 02:54:55 +0600

    Codeforces-133A-HQ9+ - Solved

---
+++ Assignment2/ProblemSolving/133A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Solution to the codeforces problem - 133A-HQ9+
+string s= Console.ReadLine();
+if(s.Contains('H') || s.Contains('Q') || s.Contains('9'))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
