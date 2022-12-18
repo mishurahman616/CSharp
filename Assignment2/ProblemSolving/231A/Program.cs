3b2481dca1822becf70c4879b086d10deb86186a
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 18:42:55 +0600

    Codeforces-231A-Team-Solved

---
+++ Assignment2/ProblemSolving/231A/Program.cs
@@ -0,0 +1,17 @@
+﻿// Solution to the codeforces problem - 231A-Team
+int n = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+for(int i=0;i<n; i++)
+{
+    var s = Console.ReadLine().Split();
+    Array.Sort(s);
+
+    if (s[1]=="1" && s[2] == "1")
+    {
+        counter++;
+    }
+
+    
+}
+
+Console.WriteLine(counter);
