47dbc62cf8bc2e287284fadda71cea7308a9b2ef
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 21:34:04 +0600

    Codeforces- 977A-Wrong Subtraction-Solved

---
+++ Assignment2/ProblemSolving/977A/Program.cs
@@ -0,0 +1,15 @@
+﻿// Solution to the codeforces problem - 977A-Wrong Subtraction
+var nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+while (nk[1]-- >=1)
+{
+    if (nk[0] % 10 == 0)
+    {
+        nk[0] /= 10;
+    }
+    else
+    {
+        nk[0]--;
+    }
+}
+Console.WriteLine(nk[0]);
