4979d356de0cca6ed38526baa1cc0ad3569607b7
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-19 01:16:39 +0600

    Codeforces-1030A-In Search of an Easy Problem-Solved

---
+++ Assignment2/ProblemSolving/1030A/Program.cs
@@ -0,0 +1,11 @@
+﻿// Solution to the codeforces problem - 1030A-In Search of an Easy Problem
+int n = Convert.ToInt32(Console.ReadLine());
+string s = Console.ReadLine();
+if(s.Contains('1'))
+{
+    Console.WriteLine("HARD");
+}
+else
+{
+    Console.WriteLine("EASY");
+}
