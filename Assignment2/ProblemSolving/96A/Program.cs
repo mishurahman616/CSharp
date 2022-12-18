f6a982a2be190b3d6f555b4bfbc94b0aa9d2d023
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 13:20:23 +0600

    Codeforces-236A-Football-Solved

---
+++ Assignment2/ProblemSolving/96A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Solution to the codeforces problem - 236A-Football
+string s = Console.ReadLine();
+if(s.Contains("0000000") || s.Contains("1111111"))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
