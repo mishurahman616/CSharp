e81501e465ede2fc6f3d2dde839d55142daa5a5a
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 19:53:53 +0600

    Codeforces- 71A-Way Too Long Words-Solved

---
+++ Assignment2/ProblemSolving/71A/Program.cs
@@ -0,0 +1,14 @@
+﻿// Solution to the codeforces problem - 71A-Way Too Long Words
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine();
+    if (s.Length > 10)
+    {
+        Console.WriteLine($"{s[0]}{s.Length-2}{s[^1]}");
+    }
+    else
+    {
+        Console.WriteLine(s);
+    }
+}
