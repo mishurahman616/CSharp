e6c0dd047582ccc6fb863dfc36de9e0f115e7ea8
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-19 01:42:29 +0600

    Codeforces-1141A-Game 23-Solved

---
+++ Assignment2/ProblemSolving/1141A/Program.cs
@@ -0,0 +1,34 @@
+﻿// Solution to the codeforces problem - 1141A-Game 23
+var data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int counter = 0;
+if (data[0] == data[1])
+{
+    Console.WriteLine(0);
+}
+else if (data[1] % data[0] != 0)
+{
+    Console.WriteLine(-1);
+}
+else
+{
+    int d = data[1] / data[0];
+    while (d %2==0)
+    {
+        d /= 2;
+        counter++;
+    }
+    while (d % 3 == 0)
+    {
+        d /= 3;
+        counter++;
+    }
+    if (d == 1)
+    {
+        Console.WriteLine(counter);
+    }
+    else
+    {
+        Console.WriteLine(-1);
+    }
+}
+
