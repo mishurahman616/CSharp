c751ae8f4edde965dec57d04e3634a14dd112fed
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-18 17:56:23 +0600

    Codeforces - 118A-String Task-Solved

---
+++ Assignment2/ProblemSolving/118A/Program.cs
@@ -0,0 +1,14 @@
+﻿// Solution to the codeforces problem - 118A-String Task
+string s = Console.ReadLine().ToLower();
+foreach(var c in s)
+{
+    if (c == 'a' || c=='o' ||c=='e' || c=='y' || c=='u' || c == 'i')
+    {
+        continue;
+    }
+    else
+    {
+        Console.Write("."+c);
+    }
+
+}
