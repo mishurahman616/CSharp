7ec5dd57c297bd63f1f1c6f080429c29f02674f2
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-19 13:25:24 +0600

    Codeforces-- 1230A-Dawid and Bags of Candies-Solved

---
+++ Assignment2/ProblemSolving/1230A/Program.cs
@@ -0,0 +1,20 @@
+﻿// Solution to the codeforces problem - 1230A-Dawid and Bags of Candies
+
+var data=Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+ Array.Sort(data);
+
+int sum1 = data[3], sum2 = data[2];
+int x = 2;
+while (--x>=0)
+{
+    if (sum1 > sum2)
+    {
+        sum2 += data[x];
+    }
+    else
+    {
+        sum1 += data[x];
+    }
+
+}
+Console.WriteLine(sum1 == sum2?"YES":"NO");
