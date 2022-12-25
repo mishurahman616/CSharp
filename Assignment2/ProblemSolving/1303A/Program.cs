b83d578660b01cd9da11c71bb1c5fa9730a64beb
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-25 15:03:27 +0600

    Codeforces-1303A-Erasing Zeroes-Solved

---
+++ Assignment2/ProblemSolving/1303A/Program.cs
@@ -0,0 +1,18 @@
+﻿// Solution to the codeforces problem - 1303A-Erasing Zeroes
+int n = Convert.ToInt32(Console.ReadLine());
+for(int j=0; j < n; j++)
+{
+    string s = Console.ReadLine();
+    string s1=s.Trim('0');
+    
+    int zeroCounter = 0;
+    for (int i = 0; i < s1.Length; i++)
+    {
+        if (s1[i] == '0')
+        {
+            zeroCounter++;
+        }
+    }
+
+    Console.WriteLine(zeroCounter);
+}
