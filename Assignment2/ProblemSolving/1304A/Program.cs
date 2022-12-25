ac10dbb964f7b01c7d7a06f5b250acd3bfbfdf4a
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-25 17:12:02 +0600

    Codeforces- 1304A-Two Rabbits-Solved

---
+++ Assignment2/ProblemSolving/1304A/Program.cs
@@ -0,0 +1,17 @@
+﻿// Solution to the codeforces problem - 1304A-Two Rabbits
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    var xyab = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+    int common = 0;
+    double commonPoint = (double)(xyab[1] - xyab[0])/ (xyab[2] + xyab[3]);
+    common = (int)commonPoint;
+    if (common == commonPoint)
+    {
+        Console.WriteLine(common);
+    }
+    else
+    {
+        Console.WriteLine(-1);
+    }
+}
