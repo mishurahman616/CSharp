5c5d375046e24559f0ec25377ccf877d0a94d140
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-26 15:13:23 +0600

    Delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,10 @@
+﻿//Delegates
+using Delegates;
+using static Delegates.DelegatesExample;
+
+DelegatesExample delegates = new DelegatesExample();
+Print print1 = new Print(delegates.PrintToFile);
+Print print2 = new Print(delegates.PrintToConsole);
+
+print1(Console.ReadLine());
+print2(Console.ReadLine());
