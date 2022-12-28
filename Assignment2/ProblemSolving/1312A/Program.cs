49c34d76181d25ed189d2a16206d4fb22d90ee3f
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-29 02:07:24 +0600

    Codeforces-1312A-Two Regular Polygons-Solved

---
+++ Assignment2/ProblemSolving/1312A/Program.cs
@@ -0,0 +1,14 @@
+﻿// Solution to the codefoces problem 1312A-Two Regular Polygons
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    var data = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+    if (data[0] % data[1] == 0)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
