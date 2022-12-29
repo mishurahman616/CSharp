db68e0781fba21b79548f7bcf8b9a83fb5fe72a2
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-29 19:31:34 +0600

    Codeforces- 25A-IQ test-Solved

---
+++ Assignment2/ProblemSolving/25A/Program.cs
@@ -0,0 +1,35 @@
+﻿//Solution to the codeforces problem - 25A-IQ test
+int n = Convert.ToInt32(Console.ReadLine());
+var data = Console.ReadLine().Trim().Split().Select(s => Convert.ToInt32(s)).ToList();
+int evenCounter = 0;
+int oddCounter = 0;
+int evenIndex =-1;
+int oddIndex = -1;
+for(int i=0; i<n; i++)
+{
+
+    if (data[i] % 2 == 0)
+    {
+        evenCounter++;
+
+        evenIndex = i;
+    }
+    if (data[i]%2 != 0)
+    {
+        
+        oddCounter++;
+
+        oddIndex = i;
+    }
+
+    if (evenCounter > 1 && oddIndex > -1)
+    {
+        Console.WriteLine(oddIndex + 1);
+        break;
+    }
+    if (oddCounter > 1 && evenIndex > -1)
+    {
+        Console.WriteLine(evenIndex + 1);
+        break;
+    }
+}
