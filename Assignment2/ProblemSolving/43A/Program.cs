027d176fd1394afd4aa965e89debab8687be1fa2
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 01:57:16 +0600

    Codeforces-43A-Football-Solved

---
+++ Assignment2/ProblemSolving/43A/Program.cs
@@ -0,0 +1,29 @@
+﻿// Solution to the codeforces problem-43A-Football
+int n = Convert.ToInt32(Console.ReadLine());
+int t1Counter=0, t2Counter=0;
+string team1, team2="";
+team1= Console.ReadLine();
+t1Counter++;
+for(int i = 1; i < n; i++)
+{
+   string team = Console.ReadLine();
+    if(team1 == team)
+    {
+        t1Counter++;
+    }
+    else
+    {
+        t2Counter++;
+        team2 = team;
+    }
+
+}
+
+if(t1Counter > t2Counter)
+{
+    Console.WriteLine(team1);
+}
+else
+{
+    Console.WriteLine(team2);
+}
