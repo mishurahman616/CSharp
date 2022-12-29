ddc8ce426f2d979e69c8ddec7f5034750d3fb5fc
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 01:19:40 +0600

    Codeforces - 34B-Sale-Solved

---
+++ Assignment2/ProblemSolving/34B/Program.cs
@@ -0,0 +1,12 @@
+﻿//Solution to the codeforces problem - 34B-Sale
+var data1 = Console.ReadLine().Trim().Split(' ').Select(s => Convert.ToInt32(s)).ToList();
+var data = Console.ReadLine().Trim().Split().Select(s=>Convert.ToInt32(s)).ToArray();
+Array.Sort(data);
+int sum = 0;
+for(int i=0; i < data1[1]; i++)
+{
+    if (data[i]<0)
+    sum += Math.Abs(data[i]);
+}
+
+Console.WriteLine(sum);
