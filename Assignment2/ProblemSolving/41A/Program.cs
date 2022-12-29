2c35a738d547bc6efbdd7ae3edd2b46c7e6bce03
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 01:44:50 +0600

    Codeforces-41A-Translation-Solved

---
+++ Assignment2/ProblemSolving/41A/Program.cs
@@ -0,0 +1,33 @@
+﻿// Solution to the codeforces problem-41A-Translation
+string s = Console.ReadLine();
+string t = Console.ReadLine();
+bool flag = true;
+if(s.Length == t.Length)
+{
+    for (int i = 0, j = s.Length - 1; i < s.Length; i++, j--)
+    {
+        if (s[i] == t[j])
+        {
+            continue;
+        }
+        else
+        {
+            flag = false;
+            break;
+        }
+    }
+}
+else
+{
+    flag = false;
+}
+
+
+if (flag)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
