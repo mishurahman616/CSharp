08b453039cf1f2eaec21ec5333d8c8ebacbeff8f
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 01:32:41 +0600

    Codeforces-405A-Gravity Flip-Solved

---
+++ Assignment2/ProblemSolving/405A/Program.cs
@@ -0,0 +1,9 @@
+﻿// Solution to the codeforces problem -405A-Gravity Flip
+
+int n = Convert.ToInt32(Console.ReadLine());
+var data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Array.Sort(data);
+foreach(var item in data)
+{
+    Console.Write(item+" ");
+}
