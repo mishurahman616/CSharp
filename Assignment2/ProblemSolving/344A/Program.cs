3307db6653f0650dd8b0849c965314e2d8c34442
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-29 20:47:49 +0600

    Codeforces-- 344A-Magnets-Solved

---
+++ Assignment2/ProblemSolving/344A/Program.cs
@@ -0,0 +1,14 @@
+﻿//Solution to the codeforces problem - 344A-Magnets
+int n = Convert.ToInt32(Console.ReadLine());
+int groupCounter = 1;
+string s1 = Console.ReadLine();
+for(int i=1; i<n; i++)
+{
+    string s2= Console.ReadLine();
+    if (!s1.Equals(s2))
+    {
+        groupCounter++;
+    }
+    s1 = s2;
+}
+Console.WriteLine(groupCounter);
