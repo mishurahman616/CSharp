353bd8a24e5bfa861ebb46660ff6936d7d694545
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 02:04:07 +0600

    Codefoces-467A-George and Accommodation-Solved

---
+++ Assignment2/ProblemSolving/467A/Program.cs
@@ -0,0 +1,12 @@
+﻿// Solution to the codeforces problem -467A-George and Accommodation
+int n = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+for(int i = 0; i < n; i++)
+{
+    var data = Console.ReadLine().Split().Select(x=>Convert.ToInt32(x)).ToArray();
+    if (data[1] - data[0] >= 2)
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);
