f67c40f528fba8c4b66944b044afeda4f2dc31d6
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-31 03:18:00 +0600

    Codeforces-579A-Raising Bacteria-Solved

---
+++ Assignment2/ProblemSolving/579A/Program.cs
@@ -0,0 +1,16 @@
+﻿// Solution to the codeforces problem -579A-Raising Bacteria
+int n = Convert.ToInt32(Console.ReadLine());
+int bacteriaCounter = 0;
+while(n > 0)
+{
+    if (n % 2 == 0)
+    {
+        n /= 2;
+    }
+    else
+    {
+        n = (n-1) / 2;
+        bacteriaCounter++;
+    }
+}
+Console.WriteLine(bacteriaCounter);
