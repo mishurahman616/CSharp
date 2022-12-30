c3d438763d6a10dab874137559e8de0b3620e6c4
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 15:22:32 +0600

    Codeforces-734A- Anton and Danik-Solved

---
+++ Assignment2/ProblemSolving/734A/Program.cs
@@ -0,0 +1,13 @@
+﻿// Solution to the codeforces problem 734A- Anton and Danik
+int n = Convert.ToInt32(Console.ReadLine());
+string s = Console.ReadLine();
+int A = 0;
+int D = 0;
+for(int i = 0; i < n; i++)
+{
+    if (s[i] == 'A') A++;
+    else D++;
+}
+if (A == D) Console.WriteLine("Friendship");
+else if (A > D) Console.WriteLine("Anton");
+else Console.WriteLine("Danik");
