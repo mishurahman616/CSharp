bd3df557e80a5354a54c60ded3193b24e00310d6
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-31 02:58:58 +0600

    Codeforces-1296A-Array with Odd Sum-Solved

---
+++ Assignment2/ProblemSolving/1296A/Program.cs
@@ -0,0 +1,27 @@
+﻿// Solution to the codeforces problem 1296A-Array with Odd Sum
+int t = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    var data = Console.ReadLine().Trim().Split().Select(s => Convert.ToInt32(s)).ToList();
+    int oddCount = 0, evenCount = 0;
+    for(int j=0; j<n; j++)
+    {
+        if (data[j]%2==0)
+        {
+            evenCount++;
+        }
+        else
+        {
+            oddCount++;
+        }
+    }
+    if(evenCount == data.Count || ((oddCount==data.Count)&& oddCount%2==0))
+    {
+        Console.WriteLine("NO");
+    }
+    else
+    {
+        Console.WriteLine("YES");
+    }
+}
