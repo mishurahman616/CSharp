872e0c6a36b5d2943da215d90996d485ef251c1e
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 15:01:26 +0600

    Codeforces - 513A - Game-Solved

---
+++ Assignment2/ProblemSolving/513A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Solution to the codeforces problem - 513A - Game
+var data = Console.ReadLine().Split().Select(x=>Convert.ToInt32(x)).ToList();
+if (data[0] > data[1])
+{
+    Console.WriteLine("First");
+}
+else
+{
+    Console.WriteLine("Second");
+}
