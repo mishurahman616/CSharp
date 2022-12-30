f2e23d2a6428403a5321151555748e34c5b631b2
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 12:59:04 +0600

    Codeforces-479A-Expression-Solved

---
+++ Assignment2/ProblemSolving/479A/Program.cs
@@ -0,0 +1,39 @@
+﻿// Solution to the codeforces problem -479A-Expression
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int c=Convert.ToInt32(Console.ReadLine());
+
+if (a == 1 && b==1 && c==1)
+{
+    Console.WriteLine(3);
+}
+else if (a == 1)
+{
+    if(c == 1)
+    {
+        Console.WriteLine(a+b+c);
+    }
+    else
+    {
+        Console.WriteLine((a + b) * c);
+    }
+}
+    
+else if(b == 1)
+{
+    if(a>c)
+    {
+        Console.WriteLine((b+c)*a);
+    }
+    else
+    {
+        Console.WriteLine((a+b)*c);
+    }
+} else if (c == 1)
+{
+    Console.WriteLine((b+c)*a);
+}
+else
+{
+    Console.WriteLine(a*b*c);
+}
