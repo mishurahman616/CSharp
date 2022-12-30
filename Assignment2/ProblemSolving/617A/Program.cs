497f67b7957983f33289ae4cd1aade034e19e8e3
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-30 15:06:16 +0600

    Codeforces- 513A - Elephant-Solved

---
+++ Assignment2/ProblemSolving/617A/Program.cs
@@ -0,0 +1,11 @@
+﻿// Solution to the codeforces problem - 513A - Elephant
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 5 == 0)
+{
+    Console.WriteLine(n / 5);
+}
+else
+{
+    Console.WriteLine((n / 5)+1);
+}
