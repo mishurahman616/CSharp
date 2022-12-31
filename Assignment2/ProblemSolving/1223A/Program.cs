fd9e845415f5aad78a935eb643477665a16a1cf4
Mishu Rahman <mishu.cse616@gmail.com>
2022-12-31 12:54:06 +0600

    Codeforces-1223A-CME-Solved

---
+++ Assignment2/ProblemSolving/1223A/Program.cs
@@ -0,0 +1,17 @@
+﻿// Solution to the codeforces problem 1223A-CME
+int q = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < q; i++)
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 4)
+    {
+        Console.WriteLine(4 - n);
+    }else if(n%2== 0)
+    {
+        Console.WriteLine(0);
+    }
+    else
+    {
+        Console.WriteLine(1);
+    }
+}
