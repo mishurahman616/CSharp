9a82c3710efa0fb7265a19889aa3c7ed93d5679c
Mishu Rahman <mishu.cse616@gmail.com>
2023-01-02 21:55:09 +0600

    Task 2

--- Exam3/Task2/Grid.cs
+++ Exam3/Task2/Grid.cs
@@ -8,6 +8,20 @@ namespace Task2
 {
     public class Grid<T> where T : struct
     {
+        public Grid(int p, int q, int r) { 
+            _grid= new T[p, q, r];
+        }
         private T[,,] _grid;
+        public T this[int x, int y, int z]
+        {
+            get
+            {
+                return _grid[x, y, z];
+            }
+            set 
+            { 
+                _grid[x, y, z]= value;
+            }
+        }
     }
 }
--- Exam3/Task2/Program.cs
+++ Exam3/Task2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Task2;
+
+Grid<int> grid = new Grid<int>(10, 10, 10);
+grid[0, 0, 0]= 1;
+Console.WriteLine(grid[0, 0, 0]);
