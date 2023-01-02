f19f5810021305a12ef5c3f767f38345d3b69a85
Mishu Rahman <mishu.cse616@gmail.com>
2023-01-02 21:02:54 +0600

    Events

---
+++ Events/Events/Button.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Button
+    {
+        public event Action<string> OnClick;
+        public void ClickButton()
+        {
+            OnClick?.Invoke("Button is clicked");
+        }
+    }
+}
